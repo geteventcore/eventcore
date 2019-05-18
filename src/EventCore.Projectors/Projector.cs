48d79b78abf9ce3ed87fe8eb1fcb6ee3caef9706
Chad Perry <chad@perryonfire.com>
2019-05-18 14:30:10 +0200

    Add default parallel key.

--- src/EventCore.Projectors/Projector.cs
+++ src/EventCore.Projectors/Projector.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using EventCore.StatefulSubscriber;
+using EventCore.Utilities;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
-using EventCore.StatefulSubscriber;
-using EventCore.Utilities;
 
 namespace EventCore.Projectors
 {
 	public abstract class Projector : IProjector,
 		ISubscriberEventSorter, ISubscriberEventHandler
 	{
+		// This can be anything except null or empty.
+		// Must have a default way to group events to be handled.
+		private const string DEFAULT_PARALLEL_KEY = ".";
+
 		private readonly IStandardLogger _logger;
 		private readonly ISubscriber _subscriber;
 
@@ -20,12 +24,20 @@ namespace EventCore.Projectors
 
 		public async Task RunAsync(CancellationToken cancellationToken)
 		{
+			_logger.LogInformation("Starting projector: " + this.GetType().Name);
+
 			await _subscriber.SubscribeAsync(cancellationToken);
+
+			_logger.LogInformation("Stopping projector: " + this.GetType().Name);
 		}
 
 		public virtual string SortSubscriberEventToParallelKey(SubscriberEvent subscriberEvent)
 		{
-			return null; // Default is no parallel key, i.e. events will not be sorted for parallel processing.
+			// Default is no parallel key, i.e. all events from the subscription stream
+			// will be handled sequentially with no optimization to execute events in parallel.
+			// The implementing class should override this is events can be consumed in paralle,
+			// for example, projecting events that are from completely separate, unrelated aggregate instances.
+			return DEFAULT_PARALLEL_KEY; // Sorting keys may not be null or empty.
 		}
 
 		public abstract Task HandleSubscriberEventAsync(SubscriberEvent subscriberEvent, CancellationToken cancellationToken);
