0822d86f4a7094e6073daf74e2c3d665bfc1b2f5
Chad Perry <chad@perryonfire.com>
2019-05-18 13:01:47 +0200

    Set up new projectors project.

---
+++ src/EventCore.Projectors/IProjector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventCore.Projectors
+{
+	public interface IProjector
+	{
+		Task RunAsync(CancellationToken cancellationToken);
+	}
+}
---
+++ src/EventCore.Projectors/Projector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventCore.Projectors
+{
+	public abstract class Projector : IProjector
+	{
+		public Task RunAsync(CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public abstract Task HandleSubscriberEventAsync();
+
+	
+	}
+}
