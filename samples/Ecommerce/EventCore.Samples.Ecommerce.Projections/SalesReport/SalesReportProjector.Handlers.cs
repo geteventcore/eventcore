dbba96a35ab78e30cc80435841b87c8d1b6a0c3c
Chad Perry <chad@perryonfire.com>
2019-05-20 14:13:13 +0200

    Add event handler for sales report projection.

---
+++ samples/Ecommerce/EventCore.Samples.Ecommerce.Projections/SalesReport/SalesReportProjector.Handlers.cs
@@ -0,0 +1,24 @@
+﻿using EventCore.Projectors;
+using EventCore.Samples.Ecommerce.Domain.Events;
+using EventCore.Samples.Ecommerce.Projections.SalesReport.SalesReportDb.DbModels;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventCore.Samples.Ecommerce.Projections.SalesReport
+{
+	public partial class SalesReportProjector :
+		IHandleBusinessEvent<SalesOrderRaisedEvent>
+	{
+		public async Task HandleBusinessEventAsync(string streamId, long position, SalesOrderRaisedEvent e, CancellationToken cancellationToken)
+		{
+			using (var scope = _dbScopeFactory.Create())
+			{
+				if (!scope.Db.ExistsSalesOrderId(e.SalesOrderId))
+				{
+					scope.Db.SalesOrder.Add(new SalesOrderDbModel() { SalesOrderId = e.SalesOrderId });
+					await scope.Db.SaveChangesAsync();
+				}
+			}
+		}
+	}
+}
--- samples/Ecommerce/EventCore.Samples.Ecommerce.Projections/SalesReport/SalesReportProjector.cs
+++ samples/Ecommerce/EventCore.Samples.Ecommerce.Projections/SalesReport/SalesReportProjector.cs
@@ -1,4 +1,5 @@
 ﻿using EventCore.Projectors;
+using EventCore.Samples.Ecommerce.Domain.Events;
 using EventCore.Samples.Ecommerce.Projections.SalesReport.SalesReportDb;
 using EventCore.StatefulSubscriber;
 using System;
@@ -7,13 +8,26 @@ using System.Threading.Tasks;
 
 namespace EventCore.Samples.Ecommerce.Projections.SalesReport
 {
-	public class SalesReportProjector : Projector
+	public partial class SalesReportProjector : Projector
 	{
 		private readonly IDbContextScopeFactory<SalesReportDbContext> _dbScopeFactory;
 
 		public SalesReportProjector(ProjectorBaseDependencies dependencies, IDbContextScopeFactory<SalesReportDbContext> dbScopeFactory) : base(dependencies.Logger, dependencies.SubscriberFactory, dependencies.StreamClientFactory, dependencies.StreamStateRepo, dependencies.EventResolver, dependencies.SubscriberFactoryOptions, dependencies.SubscriptionStreamIds)
 		{
 			_dbScopeFactory = dbScopeFactory;
+		}
+
+		public override string SortSubscriberEventToParallelKey(SubscriberEvent subscriberEvent)
+		{
+			if (subscriberEvent.IsResolved)
+			{
+				switch (subscriberEvent.ResolvedEvent)
+				{
+					case SalesOrderRaisedEvent e: return e.SalesOrderId;
+				}
+			}
+
+			return base.SortSubscriberEventToParallelKey(subscriberEvent);
 		}
 	}
 }
